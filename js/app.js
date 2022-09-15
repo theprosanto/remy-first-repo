102e2cc92a661df465f32bfdff9ba23904f96b88
theprosanto <theprosanto@gmail.com>
2022-09-15 21:33:12 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,5 @@
+
+	const incrementButton = document.getElementById("increment-btn")
+    incrementButton.addEventListener("click", function () { 
+    incrementButton.innerText = parseInt(incrementButton.innerText)+3; 
+    }) 
